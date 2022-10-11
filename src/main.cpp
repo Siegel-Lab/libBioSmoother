6b05fe67802594c59a2099018ea3ba7a6dfbe2ec
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2022-10-11 14:43:10 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "libSps/sps/abstract_index.h"
+#include "cm/version.h"
+#include "cm/build_time.h"
+
+
+PYBIND11_MODULE( libContactMapping, m )
+{
+    // prevent creation of stxxl log files
+    if( getenv( (char*)"STXXLLOGFILE" ) == nullptr )
+        putenv( (char*)"STXXLLOGFILE=/dev/null" );
+    if( getenv( (char*)"STXXLERRLOGFILE" ) == nullptr )
+        putenv( (char*)"STXXLERRLOGFILE=/dev/null" );
+
+    m.attr( "VERSION" ) = VERSION;
+    m.attr( "BUILD_TIME" ) = BUILD_TIME;
+
+}
