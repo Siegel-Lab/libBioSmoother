37e2e23fcaecc0ed86e28d3829d2c3d0c2dfc7de
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2022-10-11 14:50:41 +0200

    buildable

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
-#include "libSps/sps/abstract_index.h"
+#include "sps/abstract_index.h"
 #include "cm/version.h"
 #include "cm/build_time.h"
+
+#if WITH_PYTHON
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#endif
 
 
 PYBIND11_MODULE( libContactMapping, m )
