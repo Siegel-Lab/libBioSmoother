911927e62c415ca8d219c7e265a0a819d4511f6c
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2022-11-28 18:05:44 +0100

    report if cooler and bokeh could be imported

--- python/libContactMapping/quarry.py
+++ python/libContactMapping/quarry.py
@@ -1,8 +1,16 @@
 try:
-    from bokeh.palettes import Viridis256, Colorblind, Plasma256, Turbo256
     from .cooler_interface import icing
+    HAS_COOLER_ICING = True
 except ImportError:
     # Error handling
+    HAS_COOLER_ICING = False
+    pass
+try:
+    from bokeh.palettes import Viridis256, Colorblind, Plasma256, Turbo256
+    HAS_PALETTES = True
+except ImportError:
+    # Error handling
+    HAS_PALETTES = False
     pass
 
 from statsmodels.stats.multitest import multipletests
@@ -44,21 +52,37 @@ class Quarry(PartialQuarry):
         return "#" + zpad(hex(red)[2:]) + zpad(hex(green)[2:]) + zpad(hex(blue)[2:])
 
     def colorPalette(self, palette_name, color_low, color_high):
-        if palette_name == "Viridis256":
-            return Viridis256
-        elif palette_name == "Plasma256":
-            return Plasma256
-        elif palette_name == "Turbo256":
-            return Turbo256
-        elif palette_name == "LowToHigh":
-            return [
-                self.__combine_hex_values(
-                    {color_low[1:]: 1 - x / 255, color_high[1:]: x / 255}
-                )
-                for x in range(256)
-            ]
+        if HAS_PALETTES:
+            if palette_name == "Viridis256":
+                return Viridis256
+            elif palette_name == "Plasma256":
+                return Plasma256
+            elif palette_name == "Turbo256":
+                return Turbo256
+            elif palette_name == "LowToHigh":
+                return [
+                    self.__combine_hex_values(
+                        {color_low[1:]: 1 - x / 255, color_high[1:]: x / 255}
+                    )
+                    for x in range(256)
+                ]
+            else:
+                raise RuntimeError("invalid value for color_palette")
         else:
-            raise RuntimeError("invalid value for color_palette")
+            if palette_name == "LowToHigh":
+                return [
+                    self.__combine_hex_values(
+                        {color_low[1:]: 1 - x / 255, color_high[1:]: x / 255}
+                    )
+                    for x in range(256)
+                ]
+            else:
+                return [
+                    self.__combine_hex_values(
+                        {"000000": 1 - x / 255, "ffffff": x / 255}
+                    )
+                    for x in range(256)
+                ]
 
     @staticmethod
     def get_libSps_version():
@@ -67,3 +91,7 @@ class Quarry(PartialQuarry):
     @staticmethod
     def get_libCm_version():
         return CM_VERSION
+
+    @staticmethod
+    def has_cooler_icing():
+        return HAS_COOLER_ICING
