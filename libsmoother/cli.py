af8313f976b1ace5ac0ba1d45e38783c1be9ecdd
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-02-10 14:08:39 +0100

    made version accessible

--- libsmoother/cli.py
+++ libsmoother/cli.py
@@ -1,7 +1,13 @@
 from .indexer_parser import make_main_parser
+from ._import_lib_smoother_cpp import SPS_VERSION, LIB_SMOOTHER_CPP_VERSION
+import argparse
 
 def main():
     parser = make_main_parser()
+    parser.add_argument('-v', '--version', action='version',
+                        version=LIB_SMOOTHER_CPP_VERSION)
+    parser.add_argument('-V', '--version_sps', help=argparse.SUPPRESS, action='version',
+                        version=SPS_VERSION)
 
     args = parser.parse_args()
 
