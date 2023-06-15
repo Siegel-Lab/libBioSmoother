dbfeb024689d11392f34defcfe39c009d8b11caa
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-06-15 16:19:28 +0200

    intermediate

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ setup(
     cmdclass={"build_ext": CMakeBuild},
     extras_require={"test": "pytest"},
     zip_safe=False,
-    python_requires=">=3.9",
+    python_requires=">=3.6",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
