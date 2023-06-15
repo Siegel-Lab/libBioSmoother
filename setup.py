ffa48820c819a0cfcdd5bfe2cb20b784ab29820a
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-06-15 16:56:46 +0200

    versuin 0.1.2

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ setup(
     cmdclass={"build_ext": CMakeBuild},
     extras_require={"test": "pytest"},
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
