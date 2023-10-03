26bb58d5a5fc9c6329399f595a2c465608cdb661
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-10-03 09:59:32 +0200

    Bump version to 0.5.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sysconfig
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-VERSION = "0.5.7"
+VERSION = "0.5.8"
 
 # Convert distutils Windows platform specifiers to CMake -A arguments
 PLAT_TO_CMAKE = {
