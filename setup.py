91ee1591d220e7911d2d7ee678ef00aec5ea4813
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-10-25 15:09:46 +0200

    test

--- setup.py
+++ setup.py
@@ -60,6 +60,10 @@ class CMakeBuild(build_ext):
             cmake_args.append(
                 "-DMAX_NUM_FILTER_ANNOTATIONS=" + os.environ["LIB_BIO_SMOOTHER_MAX_NUM_FILTER_ANNOTATIONS"]
             )
+        if "SPS_WITH_STXXL" in os.environ:
+            cmake_args.append("-DWITH_STXXL =" + os.environ["SPS_WITH_STXXL"])
+        if "SPS_UNROLL_FOR_ALL_COMBINATIONS" in os.environ:
+            cmake_args.append("-DUNROLL_FOR_ALL_COMBINATIONS =" + os.environ["SPS_UNROLL_FOR_ALL_COMBINATIONS"])
         build_args = []
         # Adding CMake arguments set as environment variable
         # (needed e.g. to build for ARM OSx on conda-forge)
